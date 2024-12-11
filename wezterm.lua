040230eda9171d5ae17bf60c221b9516363edaf1
dotlnk-id2 <108515816+dotlnk-id2@users.noreply.github.com>
2024-12-11 12:10:42 +0700

    Update wezterm.lua
    wezterm cfg update

--- wezterm.lua
+++ wezterm.lua
@@ -8,10 +8,10 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'UltraDark'
 config.font =wezterm.font('JetBrains Mono', { weight = 'Bold', italic = false })
-config.font_size = 12
+config.font_size = 10
 config.window_frame = {
   font =wezterm.font('JetBrains Mono', { weight = 'Bold'}),
-  font_size = 13,
+  font_size = 10,
 }
 -- and finally, return the configuration to wezterm
 return config
