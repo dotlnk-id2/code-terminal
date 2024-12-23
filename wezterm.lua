a9eec2be79a5c2218587425efc553aa18ff17c96
fedora-e14 <fedora@qq.com>
2024-12-23 09:24:17 +0800

    wezterm v2

--- wezterm.lua
+++ wezterm.lua
@@ -13,5 +13,10 @@ config.window_frame = {
   font =wezterm.font('JetBrains Mono', { weight = 'Bold'}),
   font_size = 9,
 }
+config.hide_tab_bar_if_only_one_tab = true
+-- config.window_decorations = "RESIZE"
+config.initial_cols = 120
+config.initial_rows = 30
+config.scrollback_lines = 100000
 -- and finally, return the configuration to wezterm
 return config
